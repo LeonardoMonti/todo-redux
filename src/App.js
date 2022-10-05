bed599c67d744210f32f9178188b12bb4ec2acc2
LeonardoMonti <LeonardoMonti>
2022-10-05 00:21:57 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [task, updateTask] = useState('')
+  const [tasks, updateTasks] = useState([])
+
+  const handleInputChange = event => {
+    updateTask(event.target.value)
+  }
+
+  const handleFormSubmit = event => {
+    event.preventDefault();
+    updateTasks(oldTasks => [
+      ...oldTasks,
+      task,
+    ])
+    updateTask('')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={ handleFormSubmit }>
+        <input onChange={ handleInputChange } value={ task }/>
+        <button>Add</button>
+      </form>
+      <ul>
+        {tasks.map((t, i ) => (
+          <li key={ i }>{ t }</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
