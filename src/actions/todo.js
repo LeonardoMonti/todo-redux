97c93ece67716311f3e8a96b2ba473ef00243b47
LeonardoMonti <LeonardoMonti>
2022-10-18 03:36:08 -0300

    feat: add filter status

--- src/actions/todo.js
+++ src/actions/todo.js
@@ -6,6 +6,7 @@ const actions = {
   addTodo: createAction(types.TODO_ADD_TASK),
   removeTodo: createAction(types.TODO_REMOVE_TASK),
   updateTodo: createAction(types.TODO_UPDATE_TASK),
+  updateFilterStatus: createAction(types.TODO_UPDATE_FILTER_STATUS)
 };
 
 export { actions };
