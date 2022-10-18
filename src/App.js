9534d5745b6f9bf7f6113b53ab834350a28e55fe
LeonardoMonti <LeonardoMonti>
2022-10-18 15:06:06 -0300

    feat: create component PageTitle

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import { Toaster } from "react-hot-toast";
 
 import { Header } from "./components/Header";
 import { AppContent } from "./components/AppContent";
+import { PageTitle } from "./components/PageTitle";
+
+
+import styles from "./styles/modules/app.module.scss";
 
 function App() {
   return (
     <>
       <div className="container">
-        <Header />
-        <AppContent />
+        <PageTitle>Todo Redux</PageTitle>
+        <div className={styles.app__wrapper}>
+          <Header />
+          <AppContent />
+        </div>
       </div>
       <Toaster
         position="bottom-right"
